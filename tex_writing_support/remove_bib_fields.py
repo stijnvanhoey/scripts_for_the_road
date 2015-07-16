3cd210718d46369a6e897b8b054bc487e29ff5be
stvhoey <stvhoey.vanhoey@ugent.be>
2015-07-16 14:34:10 +0200

    added bibfile cleaning

---
+++ tex_writing_support/remove_bib_fields.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Bibtex export from Mendely cleanup
+
+Following fields are not needed to incorporate when exporting to bibtex (however default by Mendeley):
+ - file
+ - issn
+ - isbn
+ - pmid
+ - keywords
+ - url
+ - month
+ - annote
+ - language
+ - abstract
+ - mendeley-tags
+
+@author: stvhoey
+"""
+
+import sys
+
+def remove_fields(infile, outfile, fields):
+    '''
+    Remove the lines containing any of the fields listed
+
+    infile : str
+        file to modify
+    outfile : str
+        new file to create
+    fields : list of str
+        fields for which lines need to be removed
+    '''
+    # Check extension
+    if infile[-3:] != 'bib':
+        raise Exception('file for modification is not a .bib file')
+
+
+    bibfile = open(infile, "r")
+    newbibfile = open(outfile, "w")
+    for line in bibfile.readlines():
+        # check for all fields if present
+        if not sum([line.startswith(field) for field in fields]) > 0:
+            newbibfile.write(line)
+    bibfile.close()
+    newbibfile.close()
+
+
+def main(argv=None):
+    """
+
+    """
+    fields = ['file', 'issn', 'isbn', 'pmid', 'keywords', 'url', 'month',
+              'annote', 'language', 'abstract', 'mendeley-tags']
+
+    #argv[0] is always the file name itself
+    sourcefile = sys.argv[1]
+    outfile = sys.argv[2]
+    remove_fields(sourcefile, outfile, fields)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
--- tex_writing_support/remove_latexchanges.py
+++ tex_writing_support/remove_latexchanges.py
@@ -4,7 +4,7 @@ Created on Wed Apr  2 16:44:48 2014
 
 @author: stvhoey
 
-Additional poser to the changes-package in latex, to remove all \added, 
+Additional poser to the changes-package in latex, to remove all \added,
 \replaced and \deleted in the .tex-file to make new revision
 """
 
@@ -16,7 +16,7 @@ newtexfile = open("adaptedfile.tex", "w")
 for line in texfile.readlines():
     p = re.compile('\\\\replaced{ ( [^}]* ) }{ ( [^}]* ) }', re.VERBOSE)
     line = p.sub(r'\1',line)
-    
+
     q = re.compile('\\\\added{ ( [^}]* ) }', re.VERBOSE)
     line = q.sub(r'\1', line)
 
