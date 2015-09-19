3bd40ddc431a08cef10af177edccc433b7bb43f8
stvhoey <stvhoey.vanhoey@ugent.be>
2015-09-19 15:35:30 +0200

    added basic start file

--- plots/time_maps.py
+++ plots/time_maps.py
@@ -1,45 +1,83 @@
+#
+# Idea and initial implementation by Max C. Watson,
+# https://github.com/maxcw/time-maps
+# see also:
+# https://districtdatalabs.silvrback.com/time-maps-visualizing-discrete-events-across-many-timescales
+#
+# The original of Max Watson is here reimplemented
+#
+# created by S. Van Hoey
+#
+
+import datetime
 import numpy as np
+import pandas as pd
 import scipy.ndimage as ndi
 import matplotlib.pylab as plt
 
-# this code shows how to create a smoothed heatmap of a scatterplot using a Gaussian filter
-# see also http://stackoverflow.com/questions/6652671/efficient-method-of-calculating-density-of-irregularly-spaced-points
+import requests
 
-# use random xy coordinates
-xcoords = np.random.normal(0, 0.1, 10000)
-ycoords = np.random.normal(0, 0.3, 10000)
+def _get_prepost_values(timeserie):
+    """
+    """
+    return pre, post
 
-indices= (xcoords>0) & (xcoords<1) & (ycoords>0) & (ycoords<1) # indices of points between zero and one
 
-xcoords=xcoords[indices] # only use xy coords between zero and one, for simplicity
-ycoords=ycoords[indices]
+def intervals(times):
+    """
+    given an array of timestamps,
+    returns an array of separation times between the timestamps
+    """
+    shifted_times= times[:-1]
+    shifted_times = np.insert(shifted_times,0,0) ## add zero to beginning to array
+    seps= times - shifted_times
+    seps=seps[1:] # remove dummy first element
 
-plt.subplot(121) # first, a plot of the points themselves
+    return seps
 
-plt.plot(xcoords,ycoords,'b.')
+def build_time_map(timeserie):
+    """
 
-plt.xlim((0,1))
-plt.ylim((0,1))
+    Parameters
+    -----------
+    timeserie : pd.Series
 
-plt.subplot(122) # let's make a heatmap
+    """
+    xcoords, ycoords = _get_prepost_values(timeserie)
 
-Nside=1024 # this is the number of bins along x and y.
+    plt.subplot(121) # first, a plot of the points themselves
 
-H = np.zeros((Nside,Nside)) # the 'histogram' matrix that counts the number of points in each grid-square
+    plt.plot(xcoords,ycoords,'b.')
 
-x_heat = (Nside-1)*xcoords # the xy coordinates scaled to the size of the matrix
-y_heat = (Nside-1)*ycoords # subtract 1 since Python starts counting at 0, unlike Fortran and R
+    plt.xlim((0,1))
+    plt.ylim((0,1))
 
-for i in range(len(xcoords)): # loop over all points to calculate the population of each bin
-	
-	H[int(x_heat[i]), int(y_heat[i])] = H[int(x_heat[i]), int(y_heat[i])] + 1 # int() outputs an integer.
+    plt.subplot(122) # let's make a heatmap
 
-	# in Python, the above line can actually be accomplished more compactly:
-	# H[x_heat[i], y_heat[i]]  +=1
+    Nside=1024 # this is the number of bins along x and y.
 
-H = ndi.gaussian_filter(H,8) # here, 8 specifies the width of the Gaussian kernel in the x and y directions
-H = np.transpose(H) # so that the orientation is the same as the scatter plot
-# to bring out the individual points more, you can do: H=np.sqrt(H)
-plt.imshow(H, origin='lower')
+    H = np.zeros((Nside,Nside)) # the 'histogram' matrix that counts the number of points in each grid-square
 
-plt.show()
+    x_heat = (Nside-1)*xcoords # the xy coordinates scaled to the size of the matrix
+    y_heat = (Nside-1)*ycoords # subtract 1 since Python starts counting at 0, unlike Fortran and R
+
+    for i in range(len(xcoords)): # loop over all points to calculate the population of each bin
+
+    	H[int(x_heat[i]), int(y_heat[i])] = H[int(x_heat[i]), int(y_heat[i])] + 1 # int() outputs an integer.
+
+    	# in Python, the above line can actually be accomplished more compactly:
+    	# H[x_heat[i], y_heat[i]]  +=1
+
+    H = ndi.gaussian_filter(H,8) # here, 8 specifies the width of the Gaussian kernel in the x and y directions
+    H = np.transpose(H) # so that the orientation is the same as the scatter plot
+    # to bring out the individual points more, you can do: H=np.sqrt(H)
+    plt.imshow(H, origin='lower')
+
+    plt.show()
+
+
+rain = pd.read_csv("rain_data_sample.txt")
+
+
+
+
