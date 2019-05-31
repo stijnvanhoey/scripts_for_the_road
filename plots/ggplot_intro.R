2c91f12c34d05f24641d1ca208d8efacdff1fea3
stijnvanhoey <stijnvanhoey@gmail.com>
2019-05-31 11:21:59 +0200

    Update ggplot tutorial

--- plots/ggplot_intro.R
+++ plots/ggplot_intro.R
@@ -9,7 +9,7 @@
 # and other internet sources...
 ###################################################################################""
 
-setwd(dir='/media/DATA/Githubs/bw10/visualisation/testing_libraries')
+setwd(dir='~/githubs/personal_scripts_for_the_road/plots/')
 
 #A priori, get some data and make some a factor
 senicData = read.table("./senic.dat", header = T, sep = "")
@@ -33,7 +33,7 @@ abline(lsfit(senicData$age , senicData$stay), lwd=2, col = 4)
 # 2. However, ggplot brings something 'sleek' stuff
 #---------------------------------------------------------------------------
 library("ggplot2")
-qplot(senicData$age, senicData$stay) + 
+qplot(senicData$age, senicData$stay) +
   geom_smooth(method="lm",se=FALSE) +
   geom_smooth(method="loess", color="red", se=FALSE)
 
@@ -43,7 +43,7 @@ qplot(senicData$age, senicData$stay) +
 library("lattice")
 xyplot(stay~age, senicData,
        grid = TRUE,
-       type = c("p", "r", "smooth"), 
+       type = c("p", "r", "smooth"),
        auto.key=list(lines=TRUE), lwd = 3,
        col.line = "grey")
 
@@ -52,26 +52,26 @@ xyplot(stay~age, senicData,# subset=-12,
        panel = function(x, y) {
          panel.grid(h = -1, v = 2) # -1 align horiz grid to ticks, 2 indicates 2 vertical lines
          panel.xyplot(x, y, col='black')
-         panel.loess(x, y, span=0.5, col='red')  # Span is the loess smoothness parameter, default 2/3 
-         panel.lmline(x, y, col='blue')  # Adds a least squares line 
+         panel.loess(x, y, span=0.5, col='red')  # Span is the loess smoothness parameter, default 2/3
+         panel.lmline(x, y, col='blue')  # Adds a least squares line
        } )
 #looks like nested-mathematica-stuff?!??
 
 #---------------------------------------------------------------------------
 # 4. doing + for putting something extra is more intuitive -> ggplot2!
 #---------------------------------------------------------------------------
-qplot(senicData$age, senicData$stay) + 
-  geom_smooth(, method="lm", se=FALSE) +
-  geom_smooth(, method="loess", se=FALSE, col = "red") +
-  xlab("Age of patient (years)") + 
+qplot(senicData$age, senicData$stay) +
+  geom_smooth(method="lm", se=FALSE) +
+  geom_smooth(method="loess", se=FALSE, col = "red") +
+  xlab("Age of patient (years)") +
   ylab("Duration of stay (days)") +
   ggtitle("Duration in function of age")
 
 #...except of legends; however you have to get the aes-idea (just in a minute!)
-qplot(senicData$age, senicData$stay) + 
+qplot(senicData$age, senicData$stay) +
   geom_smooth(aes(colour="linear fit"), method="lm", se=FALSE) +
   geom_smooth(aes(colour="loess fit"), method="loess", se=FALSE) +
-  xlab("Age of patient (years)") + 
+  xlab("Age of patient (years)") +
   ylab("Duration of stay (days)") +
   ggtitle("Duration in function of age") +
   scale_colour_discrete("")
@@ -99,18 +99,18 @@ xyplot(stay~age, senicData,
 ggplot(senicData, aes(x = age, y = stay, shape = region, color = beds)) + geom_point(size = 5)
 
 # 2 FACTORS CREATED... split it:
-ggplot(senicData, aes(x = age, y = stay)) + 
+ggplot(senicData, aes(x = age, y = stay)) +
   geom_point(size = 2) +
   geom_smooth(method="lm") +
   facet_grid(~region)
-             
 
-ggplot(senicData, aes(x = age, y = stay)) + 
+
+ggplot(senicData, aes(x = age, y = stay)) +
          geom_point(size = 2) +
          facet_grid(region~school)
 
 # 2 FACTORS CREATED... regressions, just +...
-ggplot(senicData, aes(x = age, y = stay)) + 
+ggplot(senicData, aes(x = age, y = stay)) +
   geom_point(size = 2) +
   facet_grid(~region) +
   stat_smooth(method = "lm", se = TRUE) +
@@ -121,10 +121,10 @@ ggplot(senicData, aes(x = age, y = stay)) +
 # 6. you just used qplot and ggplot...?!? -> OK, some theory!
 #---------------------------------------------------------------------------
 
-# The basic concept of a ggplot2 graphic in R is that you combine different elements into layers. 
+# The basic concept of a ggplot2 graphic in R is that you combine different elements into layers.
 
 # qplot vs ggplot => qplot is 'quick-plot'; ggplot more generic
-# run: ggplot(senicData, aes(x = age, y = stay)) 
+# run: ggplot(senicData, aes(x = age, y = stay))
 #=> no layers... points need to be added as layer (geom_point)
 #and:
 qplot(age, data=senicData, geom="histogram")
@@ -135,13 +135,13 @@ ggplot(data=senicData) + geom_histogram(aes(x=age))
 
 # DATA
 #   * The data that you want to plot: For ggplot(), this must be a data frame; eg senicData
-#   * A mapping from the data to your plot: This usually is as simple as telling ggplot() 
+#   * A mapping from the data to your plot: This usually is as simple as telling ggplot()
 #     what goes on the x-axis and what goes on the y-axis; eg aes(x = age, y = stay);
 #     this can be added to the ggplot-object, but also to a specific layer
 # LAYERS
-#   * A geometric object, or geom in ggplot terminology: The geom defines the overall look of 
+#   * A geometric object, or geom in ggplot terminology: The geom defines the overall look of
 #     the layer (for example, whether the plot is made up of bars, points, or lines); eg geom_point
-#   * A statistical summary, called a stat in ggplot: This describes how you want the data to be 
+#   * A statistical summary, called a stat in ggplot: This describes how you want the data to be
 #     summarized (for example, binning for histograms, or smoothing to draw regression lines); stat_smooth
 # => if data and aes() in ggplot object, geom_XXX just referring to this data and aes; but can also be specified in layer!
 # FACETS
@@ -157,7 +157,7 @@ ggplot(data=senicData) + geom_histogram(aes(x=age))
 #---------------------------------------------------------------------------
 # 7. Some initial learning curve details
 #---------------------------------------------------------------------------
-# A. aesthetics etc... 
+# A. aesthetics etc...
 #-----------------------
 #assigning <> setting!! => check following difference
 ggplot(data=senicData) + geom_density(aes(x=age, color=school))
@@ -170,12 +170,12 @@ ggplot(data=senicData) + geom_density(aes(x=age), color="blue")
 # 2. size witint aes
 # 4. facet_XXX
 #eg.
-ggplot(senicData, aes(x = age, y = stay, shape = region, color = beds, size = nurses)) + 
+ggplot(senicData, aes(x = age, y = stay, shape = region, color = beds, size = nurses)) +
   geom_point() +
   facet_wrap(~school)
 #I'm not saying it makes things more clear...
 #...maybe this is already better:
-ggplot(senicData, aes(x = age, y = stay, color = beds, size = nurses)) + 
+ggplot(senicData, aes(x = age, y = stay, color = beds, size = nurses)) +
   geom_point() +
   facet_grid(region~school) +
   theme_bw()
@@ -183,11 +183,11 @@ ggplot(senicData, aes(x = age, y = stay, color = beds, size = nurses)) +
 # B. scale options !!
 #-----------------------
 #Following two plots are not at all the same outcome
-qplot(senicData$age, model1$res^2) + 
+qplot(senicData$age, model1$res^2) +
   geom_smooth(method="lm", se=FALSE) +
   geom_smooth(method="loess",  color="red", se=FALSE) +
   coord_cartesian(ylim=c(0, 10))
-qplot(senicData$age, model1$res^2, ylim=c(0, 10)) + 
+qplot(senicData$age, model1$res^2, ylim=c(0, 10)) +
   geom_smooth(method="lm", se=FALSE) +
   geom_smooth(method="loess",  color="red", se=FALSE)
 #The second one is partly neglecting the data... luckily R/ggplot warns us for this!
@@ -204,7 +204,7 @@ ggQQ.lm <- function(LM) # argument: a linear model
   p <- ggplot(LM, aes(sample=.resid)) +
     stat_qq(alpha = 0.5) +
     geom_abline(slope = slope, intercept = int, color="black")
-  
+
   return(p)
 }
 
@@ -222,7 +222,7 @@ ggQQ.lm(model1)
 # we have seen scatterplots, histograms, density plots so far.
 
 # add rugs to the density or histogram plot
-ggplot(data=senicData, aes(x=nurses, fill=school)) + geom_histogram()  + geom_rug(aes(color=school)) 
+ggplot(data=senicData, aes(x=nurses, fill=school)) + geom_histogram()  + geom_rug(aes(color=school))
 #(make sure you get the aes of the previous plot)
 
 #boxplot:
@@ -233,24 +233,24 @@ se=0.4 #normally these are not just a fixed value
 ggplot(data=senicData, aes(x=age, y=stay)) + geom_point() + geom_errorbar(aes(ymax = stay + se, ymin=stay - se))
 
 #2D density
-ggplot(data=senicData, aes(x=age, y=stay))+ geom_point() + geom_density2d(aes(color=region)) + geom_rug(col="blue") 
+ggplot(data=senicData, aes(x=age, y=stay))+ geom_point() + geom_density2d(aes(color=region)) + geom_rug(col="blue")
 
 #AND OTHERS: http://docs.ggplot2.org/current/
 # geom_abline  	geom_jitter
 # geom_area		geom_line
-# geom_bar		geom_linerange 
-# geom_bin2d		geom_path 
-# geom_blank		geom_point 
-# geom_boxplot		geom_pointrange 
-# geom_contour		geom_polygon 
-# geom_crossbar		geom_quantile 
-# geom_density		geom_rect 
+# geom_bar		geom_linerange
+# geom_bin2d		geom_path
+# geom_blank		geom_point
+# geom_boxplot		geom_pointrange
+# geom_contour		geom_polygon
+# geom_crossbar		geom_quantile
+# geom_density		geom_rect
 # geom_density2d		geom_ribbon
-# geom_errorbar		geom_rug 
-# geom_errorbarh		geom_segment 
-# geom_freqpoly		geom_smooth 
-# geom_hex		geom_step 
-# geom_histogram		geom_text 
+# geom_errorbar		geom_rug
+# geom_errorbarh		geom_segment
+# geom_freqpoly		geom_smooth
+# geom_hex		geom_step
+# geom_histogram		geom_text
 # geom_hline		geom_tile
 # geom_vline
 
@@ -274,7 +274,7 @@ ggplot(data=senicData, aes(x=age, y=stay)) + stat_quantile(aes(color=region), qu
 ggplot(data=senicData, aes(x=age, y=stay)) + geom_quantile(aes(color=region), quantile=q10) + geom_point()
 
 # adding summary satistics...
-ggplot(data=senicData, aes(x=region, y=stay))+ 
+ggplot(data=senicData, aes(x=region, y=stay))+
   geom_point() +
   stat_summary(fun.data = "mean_cl_boot", colour = "red", lwd=1) #Hmisc package
 
@@ -311,7 +311,7 @@ putPlot(plotm, personal_plot, 1, 3)
 
 library("ggthemes")
 
-ggplot(senicData, aes(x = age, y = stay, color=region)) + 
+ggplot(senicData, aes(x = age, y = stay, color=region)) +
   geom_smooth(aes(colour=region), method='lm') +
   geom_point() + geom_rug() +
   theme_wsj()
@@ -339,14 +339,14 @@ ggplot(senicData, aes(x = age, y = stay, color=region)) +
 #---------------------------------------------------------------------------
 # GGPLOT IS ALSO AVAILABLE FOR PYTHON => http://ggplot.yhathq.com/
 # + ggplot, seaborn, bokeh,...
-# 
+#
 # EXAMPLE TODO
 
 
 #---------------------------------------------------------------------------
 # 10. To conclude:
 #---------------------------------------------------------------------------
-# IF(!) you have a dataframe like datatable with some factors (multivariate/categorical data) 
+# IF(!) you have a dataframe like datatable with some factors (multivariate/categorical data)
 # => ggplot is really the way to check the data, fit statsitical models, etc...
 #...otherwise...just one of the many plot options!
 
